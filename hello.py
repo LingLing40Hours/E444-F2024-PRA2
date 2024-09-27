f678a9d09396bce3efb246e3f19b1a516d238aad
Xiangyu Chen <chenxy327@gmail.com>
2024-09-27 16:36:10 -0400

    finish activity 1.4

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Regexp
 
 
 app = Flask(__name__)
@@ -15,6 +15,11 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[
+        DataRequired(),
+        Email("Email must contain '@'"),
+        Regexp(r'.*utoronto.*', message="email must be UofT address"),
+    ])
     submit = SubmitField('Submit')
 
 
@@ -23,11 +28,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
